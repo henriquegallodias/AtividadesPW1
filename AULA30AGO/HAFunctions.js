cf54899bce63c0f9906f230d532e53cfa8241504
henriquegallodias <henrique0521@hotmail.com>
2018-08-30 20:36:16 -0300

    debugger

---
+++ AULA30AGO/HAFunctions.js
@@ -0,0 +1,9 @@
+function multiplicador(a,b,c) {
+	console.log("function multiplicador(a,b,c){...");
+	var resultado = a*b*c;
+	console.log("imprimindo resultado = a*b*c");
+	return a*b*c ;
+	}
+var resultconta = multiplicador("5","5","5");	
+alert( resultconta );
+	
