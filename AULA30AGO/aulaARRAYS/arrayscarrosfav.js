baef2d050163ed100060efa54c896a4634034f7f
henriquegallodias <henrique0521@hotmail.com>
2018-08-30 22:40:57 -0300

    ativ arraus

---
+++ AULA30AGO/aulaARRAYS/arrayscarrosfav.js
@@ -0,0 +1,17 @@
+function VerCarrosH(arrCarrosH) {
+  console.log(arrCarrosH);
+}
+var carrosX = ["skylineR34","bugatti","aston martin","lamborghini veneno","uno de firma"];
+VerCarrosH(carrosX);
+
+function CarrosDois(Diversos) {
+   var i ;
+   for(i = 0;i < Diversos.lenght;i++) {
+    console.log( Diversos[i]);
+   }
+}
+var carros_rapidos = ["skylineR34s","fgsdgfsd","gesrg"];
+
+CarrosDois(carros_rapidos);
+console.log("quantidade de carros : "+carros_rapidos.lenght);
+   
